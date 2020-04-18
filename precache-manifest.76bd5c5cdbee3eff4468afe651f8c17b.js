562ab4e74210016a9338550322b40aad7ed0546c
“manassuri7” <manassuri7@outlook.com>
2020-04-18 15:43:59 +0530

    Updates

---
+++ precache-manifest.76bd5c5cdbee3eff4468afe651f8c17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af09b6af75b9131c6303e1cee89927e7",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "7537a3daf930c04ed60f",
+    "url": "/covid19tracker/static/css/main.1d4c2760.chunk.css"
+  },
+  {
+    "revision": "dba5bfceef17f14f4146",
+    "url": "/covid19tracker/static/js/2.745d68c8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.745d68c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7537a3daf930c04ed60f",
+    "url": "/covid19tracker/static/js/main.fb11dc14.chunk.js"
+  },
+  {
+    "revision": "8c436b782a43d7cb88dc",
+    "url": "/covid19tracker/static/js/runtime-main.71a8fd2c.js"
+  }
+]);
